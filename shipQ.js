0e4273120823a70bc9c5749cafc794bc8346e9f4
Danny Scott <dannyrscott@gmail.com>
2013-05-29 14:31:27 -0400

    q example

--- shipQ.js
+++ shipQ.js
@@ -4,34 +4,34 @@ var Ship = function() {
 		Fry: {
 			loadDelivery: function(cb) {
 				console.log('loading delivery');
-				return $.ajax({
+				return Q.when($.ajax({
 					data: {error:false,wait:1},
 					url: 'fakeDB.cfm',
 					type: "GET",
 					dataType: "json"
-				});
+				}));
 			}
 		},
 		Bender: {
 			prepareMeal: function() {
 				console.log('preparing meal');
-				return $.ajax({
+				return Q.when($.ajax({
 					data: {error:true,wait:2},
 					url: 'fakeDB.cfm',
 					type: "GET",
 					dataType: "json"
-				});
+				}));
 			}
 		},
 		Leela: {
 			plotCourse: function() {
 				console.log('plotting course');
-				return $.ajax({
+				return Q.when($.ajax({
 					data: {error:false,wait:1},
 					url: 'fakeDB.cfm',
 					type: "GET",
 					dataType: "json"
-				});
+				}));
 			}
 		}
 	},
